923af0838c85db35de795a55d37b8cf6c0644ced
Philipp Böhm <philipp-boehm@live.de>
2012-03-28 12:39:06 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auditorium::Application.config.secret_token = '2586c8da8be495ef166ade903ec7317d939466edaa8570647a34cff67b86596ee6037886ce90dc7117584a4321748cbd81b9e83a4818203fe95081c38b0cbfa2'
