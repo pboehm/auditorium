ddb4125429e8c8ef84905a978fbd83689f2962e3
Philipp Böhm <philipp-boehm@live.de>
2012-04-06 00:15:16 +0200

    i18n fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,5 @@ Auditorium::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.i18n.available_locales = :nl
+  config.i18n.available_locales = :de
 end
