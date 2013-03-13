960c64c78a17f746aaf1674f10097251299b2696
Philipp Böhm <philipp-boehm@live.de>
2013-03-13 18:08:55 +0100

    Changed Mail settings to use auditorium@pboehm.org

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,28 +1,19 @@
 class UserMailer < ActionMailer::Base
-  default from: "auditorium@i77i.de"
+  default from: "auditorium@pboehm.org"
 
   # Public: Sends a notification about a new post to a user
   #
   # user - User that should receive the notification
   # post - post that should be delivered to the user
   #
-  def new_post_notification(user, post)
-    @user = user
+  def send_notification_to_user(post)
     @post = post
 
-    mail(:to => "#{user.name} <#{user.email}>",
-         :subject => "Auditorium Breaking News (Neue Diskussion)")
-  end
+    recipients = User.find_all_by_notify_new_posts(true).
+                      select {|u| u != post.user }.
+                      map {|u| "#{u.name} <#{u.email}>"}
 
-  # Public: finds the user that should be notified
-  #
-  # post - post that the user should be notified
-  def send_notification_to_user(post)
-
-    User.find_all_by_notify_new_posts(true).each do |user|
-      next if user == post.user
-
-      UserMailer.new_post_notification(user, post).deliver
-    end
+    mail(:bcc => recipients,
+         :subject => "Auditorium Breaking News (Neue Diskussion)").deliver
   end
 end
